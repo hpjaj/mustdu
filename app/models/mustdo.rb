7c35fbf5a427a8c74a2d80fefbd18b00829e3fb0
hpjaj <hmlevine@gmail.com>
2014-09-25 09:19:19 -0600

    Checked complete deletes the row from the db. Working to get it to hide instead.

--- app/models/mustdo.rb
+++ app/models/mustdo.rb
@@ -2,7 +2,7 @@ class Mustdo < ActiveRecord::Base
 
   belongs_to :user
   
-  default_scope { order('created_at DESC') }
+  default_scope { order('created_at ASC') }
 
   def calculates_mustdos_days_left
     mustdo.created_at + 7.days - Time.now

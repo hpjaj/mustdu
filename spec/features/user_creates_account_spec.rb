2bcb69b322e84f46bcce953da5d3bfa7357f7c1d
hpjaj <hmlevine@gmail.com>
2014-10-03 11:34:12 -0600

    Fixed all the features specs except the mark_items_complete spec

--- spec/features/user_creates_account_spec.rb
+++ spec/features/user_creates_account_spec.rb
@@ -3,13 +3,17 @@ require 'rails_helper'
 describe 'Registering' do
   it 'creates a new user account' do
     visit '/'
-    click_link 'Sign up'
+
+    within '.pull-right' do
+        click_link 'Sign Up'
+    end
+
     visit new_user_registration_path
     fill_in 'Enter name', with: 'John Smith'
     fill_in 'Enter email', with: 'John@example.com'
     fill_in 'Enter password', with: 'password'
     fill_in 'Enter password confirmation', with: 'password'
-    click_button 'Sign up'
+    click_link 'Sign Up'
   end
 
 end

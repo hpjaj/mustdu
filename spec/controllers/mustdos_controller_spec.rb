a9e117437c9942f054d4e810633994c20421ca79
hpjaj <hmlevine@gmail.com>
2014-10-05 10:37:19 -0600

    Uncommented spec code

--- spec/controllers/mustdos_controller_spec.rb
+++ spec/controllers/mustdos_controller_spec.rb
@@ -10,22 +10,22 @@ RSpec.describe MustdosController, :type => :controller do
     sign_in @user
   end
 
-  # describe "GET index" do
-  #   it "returns http success" do
-  #     get :index
-  #     expect(response).to have_http_status(:success)
-  #   end
+  describe "GET index" do
+    it "returns http success" do
+      get :index
+      expect(response).to have_http_status(:success)
+    end
 
-  #   it "finds current_user's mustdos, where complete is false, and assigns the array to @mustdos" do
-  #     get :index
-  #     expect(Mustdo.find_by_complete(false).description).to eq("This item needs to get done this week")
-  #   end
+    it "finds current_user's mustdos, where complete is false, and assigns the array to @mustdos" do
+      get :index
+      expect(Mustdo.find_by_complete(false).description).to eq("This item needs to get done this week")
+    end
 
-  #   it "assigns @musdo" do
-  #     get :index
-  #     expect(assigns(:mustdo)).to be_a_new(Mustdo)
-  #   end
-  # end
+    it "assigns @musdo" do
+      get :index
+      expect(assigns(:mustdo)).to be_a_new(Mustdo)
+    end
+  end
 
 
   describe "POST create" do
@@ -47,35 +47,35 @@ RSpec.describe MustdosController, :type => :controller do
   end
 
 
-  # describe "GET edit" do
-  #   xit "returns http success" do
-  #     get :edit
-  #     expect(response).to have_http_status(:success)
-  #   end
-  # end
+  describe "GET edit" do
+    xit "returns http success" do
+      get :edit
+      expect(response).to have_http_status(:success)
+    end
+  end
 
 
-  # describe "GET show" do
-  #   xit "returns http success" do
-  #     get :show
-  #     expect(response).to have_http_status(:success)
-  #   end
-  # end
+  describe "GET show" do
+    xit "returns http success" do
+      get :show
+      expect(response).to have_http_status(:success)
+    end
+  end
 
 
-  # describe "GET update" do
-  #   xit "returns http success" do
-  #     get :update
-  #     expect(response).to have_http_status(:success)
-  #   end
-  # end
+  describe "GET update" do
+    xit "returns http success" do
+      get :update
+      expect(response).to have_http_status(:success)
+    end
+  end
 
 
-  # describe "GET destroy" do
-  #   xit "returns http success" do
-  #     get :destroy
-  #     expect(response).to have_http_status(:success)
-  #   end
-  # end
+  describe "GET destroy" do
+    xit "returns http success" do
+      get :destroy
+      expect(response).to have_http_status(:success)
+    end
+  end
 
 end

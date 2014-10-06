ff5edaf4690d66acf7e3856cf0f48bb094440dc6
Eliot Sykes <eliotsykes@gmail.com>
2014-10-06 10:26:20 +0100

    Test Mustdo validations

--- spec/models/mustdo_spec.rb
+++ spec/models/mustdo_spec.rb
@@ -10,4 +10,12 @@ RSpec.describe Mustdo, :type => :model do
 
   end
 
+  describe "validations" do
+
+    it "ensures description is at least 5 characters" do
+      expect(Mustdo.new).to ensure_length_of(:description).is_at_least(5)
+    end
+
+  end
+
 end

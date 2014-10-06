617840c4fa0ee6eddae8f9bce33d5f11fbbe3777
Eliot Sykes <eliotsykes@gmail.com>
2014-10-06 10:55:07 +0100

    TimeHelpers made available to specs (travel, travel_to, travel_back)

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -31,8 +31,11 @@ RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
-  # Make Factory Girl's methods avilable
+  # Make Factory Girl's methods available
   config.include FactoryGirl::Syntax::Methods
+
+  # Make the time-travel helpers available to specs
+  config.include ActiveSupport::Testing::TimeHelpers
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false

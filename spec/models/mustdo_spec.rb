99e74348f09f51f1b7d3f682f11a63647f6db539
Eliot Sykes <eliotsykes@gmail.com>
2014-10-06 10:23:49 +0100

    Test Mustdo<->User associations

--- spec/models/mustdo_spec.rb
+++ spec/models/mustdo_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Mustdo, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "associations" do
+
+    it "belongs to user" do
+      expect(Mustdo.new).to belong_to(:user)
+    end
+
+  end
+
 end

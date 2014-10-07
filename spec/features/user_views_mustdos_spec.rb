708305fe612d5c3a4e446cc796ba2e28bb08b05c
Eliot Sykes <eliotsykes@gmail.com>
2014-10-07 21:59:06 +0100

    Prompt user to create mustdo if they have none

--- spec/features/user_views_mustdos_spec.rb
+++ spec/features/user_views_mustdos_spec.rb
@@ -1,14 +1,30 @@
 require "rails_helper"
 
 feature "User views mustdos" do
+  include Warden::Test::Helpers
+  Warden.test_mode!
 
-  scenario "redirects to login if not logged in" do
+  after { Warden.test_reset! }
+
+  scenario "redirect to login if not logged in" do
     
     visit mustdos_path
 
     expect(page.current_path).to eq new_user_session_path
     expect(page).to have_content "You need to sign in or sign up before continuing."
-    
+
   end
+
+  scenario "prompt to create mustdo when list empty" do
+
+    user = create(:user)
+    login_as(user, :scope => :user)
+    visit mustdos_path
+
+    expect(page).to have_content "You've got no mustdos, why not create one?"
+
+  end
+
+
 
 end

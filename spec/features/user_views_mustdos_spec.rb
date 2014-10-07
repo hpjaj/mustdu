faaff9df7e78a2f0b45b869f4377f13186db38e4
Eliot Sykes <eliotsykes@gmail.com>
2014-10-07 21:42:42 +0100

    Test anonymous users are asked to sign in/up after being redirected to sign in page

--- spec/features/user_views_mustdos_spec.rb
+++ spec/features/user_views_mustdos_spec.rb
@@ -7,6 +7,7 @@ feature "User views mustdos" do
     visit mustdos_path
 
     expect(page.current_path).to eq new_user_session_path
+    expect(page).to have_content "You need to sign in or sign up before continuing."
     
   end
 
